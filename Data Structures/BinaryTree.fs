91106264dd81308322222124730bf08fdb8241ba
Piotr Kosiński <piotrkosinski@gmail.com>
2016-04-19 13:03:38 +0200

    BinaryTree type with Empty option. Redundant single Element removed.

--- Data Structures/BinaryTree.fs
+++ Data Structures/BinaryTree.fs
@@ -1,12 +1,12 @@
 ﻿module BinaryTree
 
 type BinaryTree<'a when 'a:comparison> = 
-    | Element of 'a
+    | Empty
     | Tree of BinaryTree<'a> * 'a * BinaryTree<'a>
 
 let rec isMember (element :'a) (tree :BinaryTree<'a>) =
     match tree with 
-    | Element a -> a = element
+    | Empty -> false
     | Tree (left, top, right) -> 
         if top < element then isMember element right
         else if top > element then isMember element left
--- DataStructure.Tests/BinaryTreeTest.fs
+++ DataStructure.Tests/BinaryTreeTest.fs
@@ -4,20 +4,21 @@ open BinaryTree
 open NUnit.Framework
 open Swensen.Unquote
 
-let singleNode = Element 4
-let simpleTree = Tree (Element 4, 6, Element 7)
+let createNode value = Tree (Empty, value, Empty)
+let simpleTree = Tree (createNode(5) , 6, createNode(7))
 
 [<Test>]
 let ``isMember given element 4 and singleNode 4 returns true`` () =
-    isMember 4 singleNode =! true
+    let tree = (createNode 4)
+    isMember 4  tree =! true
 
 [<Test>]
 let ``isMember given element 8 and simpleTree (does not contain 8) returns false`` () =
     isMember 8 simpleTree =! false
 
 [<Test>]
-let ``isMember given element 4 and simpleTree (4 is in left branch) returns true`` () =
-    isMember 4 simpleTree =! true
+let ``isMember given element 4 and simpleTree (5 is in left branch) returns true`` () =
+    isMember 5 simpleTree =! true
 
 [<Test>]
 let ``isMember given element 6 and simpleTree (6 is at the top) returns true`` () =
