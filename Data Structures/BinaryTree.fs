67494162a60f9b5c69c35af645b65f9e98dc16ae
Jarosław Krefta <Jaroslaw.Krefta@powel.com>
2016-04-19 11:36:25 +0200

    Result of session with BinaryTree

---
+++ Data Structures/BinaryTree.fs
@@ -0,0 +1,13 @@
+﻿module BinaryTree
+
+type Tree<'a when 'a:comparison> = 
+    | TreeElement of 'a
+    | BinaryTree of Tree<'a> * 'a * Tree<'a>
+
+let rec treeMember (element :'a) (tree :Tree<'a>) =
+    match tree with 
+    | TreeElement a -> a = element
+    | BinaryTree (left, top, right) -> 
+        if top < element then treeMember element right
+        else if top > element then treeMember element left
+        else true
---
+++ DataStructure.Tests/BinaryTreeTest.fs
@@ -0,0 +1,24 @@
+﻿module BinaryTreeTest
+
+open BinaryTree
+open NUnit.Framework
+open Swensen.Unquote
+
+let singleNode = TreeElement 4
+let simpleTree = BinaryTree (TreeElement 4, 6, TreeElement 7)
+
+[<Test>]
+let ``treeMember given element 4 and singleNode 4 returns true`` () =
+    treeMember 4 singleNode =! true
+
+[<Test>]
+let ``treeMember given element 8 and simpleTree (does not contain 8) returns false`` () =
+    treeMember 8 simpleTree =! false
+
+[<Test>]
+let ``treeMember given element 4 and simpleTree (4 is in left branch) returns true`` () =
+    treeMember 4 simpleTree =! true
+
+[<Test>]
+let ``treeMember given element 6 and simpleTree (6 is at the top) returns true`` () =
+    treeMember 6 simpleTree =! true
