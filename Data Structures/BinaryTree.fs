cc7e725013aab8bd2b3ee54c30ebec8b96febd15
Piotr Kosiński <piotrkosinski@gmail.com>
2016-04-21 11:12:13 +0200

    BinaryTree isMember with D+1 comparisons.

--- Data Structures/BinaryTree.fs
+++ Data Structures/BinaryTree.fs
@@ -1,13 +1,39 @@
 ﻿module BinaryTree
 
-type BinaryTree<'a when 'a:comparison> = 
+type BinaryTree<'a when 'a : comparison> = 
     | Empty
     | Tree of BinaryTree<'a> * 'a * BinaryTree<'a>
 
-let rec isMember (element :'a) (tree :BinaryTree<'a>) =
-    match tree with 
+// 2d comparisons
+let rec isMember (element : 'a) (tree : BinaryTree<'a>) = 
+    match tree with
     | Empty -> false
-    | Tree (left, top, right) -> 
+    | Tree(left, top, right) -> 
         if top < element then isMember element right
         else if top > element then isMember element left
         else true
+
+// 2d comparisons
+let rec insert (element : 'a) (tree : BinaryTree<'a>) = 
+    match tree with
+    | Empty -> Tree(Empty, element, Empty)
+    | Tree(left, top, right) -> 
+        if top < element then insert element right
+        else if top > element then insert element left
+        else tree
+
+// d + 1 comparisons
+let isMember2 (element : 'a) (tree : BinaryTree<'a>) = 
+    let rec isMember2 (element : 'a) (candidate : 'a) (tree : BinaryTree<'a>) = 
+        match tree with
+        | Empty -> candidate = element
+        | Tree(left, top, right) -> 
+            if top < element then isMember2 element candidate right
+            else isMember2 element top left
+    
+    let top tree = 
+        match tree with
+        | Tree(_, top, _) -> top
+        | _ -> raise (System.Exception "Fuck you")
+    
+    isMember2 element (top tree) tree
--- DataStructure.Tests/BinaryTreeTest.fs
+++ DataStructure.Tests/BinaryTreeTest.fs
@@ -23,3 +23,25 @@ let ``isMember given element 4 and simpleTree (5 is in left branch) returns true
 [<Test>]
 let ``isMember given element 6 and simpleTree (6 is at the top) returns true`` () =
     isMember 6 simpleTree =! true
+
+[<Test>]
+let ``insert given element 9 and simpleTree return tree with 9 added in node 7`` () =
+    let result = insert 9 simpleTree
+    isMember 9 result =! true
+
+[<Test>]
+let ``isMember2 given element 4 and singleNode 4 returns true`` () =
+    let tree = (createNode 4)
+    isMember2 4  tree =! true
+
+[<Test>]
+let ``isMember2 given element 8 and simpleTree (does not contain 8) returns false`` () =
+    isMember2 8 simpleTree =! false
+
+[<Test>]
+let ``isMember2 given element 4 and simpleTree (5 is in left branch) returns true`` () =
+    isMember2 5 simpleTree =! true
+
+[<Test>]
+let ``isMember2 given element 6 and simpleTree (6 is at the top) returns true`` () =
+    isMember2 6 simpleTree =! true    
